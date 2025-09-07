bb2f78be0471a89402ecbca1e264c04df805bf09
MadsRantzau49 <madsrantzau2nd@gmail.com>
2025-09-07 15:12:51 +0200

    no sql run in main

--- src/database/sql.rs
+++ src/database/sql.rs
@@ -5,7 +5,7 @@ pub fn run_sql(conn: &Connection) -> Result<(), rusqlite::Error> {
     "
             UPDATE odds
             SET is_gain_freebet = 0
-            WHERE is_gain_freebet = 1;
+            WHERE is_gain_freebet = 1 AND id != 5;
         ",
         params![],
     )?;
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ fn main() {
     let conn = database::establish_connection().expect("DB failed");
     database::init_db(&conn).expect("DB init failed");
     //Run SQL Code:
+    /*
     match sql::run_sql(&conn){
         Ok(_) => {}
         Err(e) => {
             println!("{e}");
         }
     }
+    */
 
 
     server::run("0.0.0.0:8000").expect("Server failed");
